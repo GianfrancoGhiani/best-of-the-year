4062cd8477a1b6a4f87161cc2ff57a6fdafd193e
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-03-27 16:11:45 +0200

    add a redirect if the searching id doesnt match

--- src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
+++ src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,25 +62,31 @@ public class HomeController {
     }
 
     @GetMapping("/movies/{id}")
-    public String getMovie(@PathVariable("id") int id, Model model){
+    public String getMovie(@PathVariable("id") int id, Model model, RedirectAttributes redirectAttributes){
         for (Movie m: getBestMovies()) {
             if (m.getId() == id){
                 model.addAttribute("mediaId", m.getId());
                 model.addAttribute("mediaTitle", m.getTitle());
                 model.addAttribute("movieDirector", m.getDirector());
                 model.addAttribute("movieDuration", m.getMinDuration());
+            } else{
+                redirectAttributes.addFlashAttribute("message", "You was serching for a not listed movie pls retry");
+                return "redirect:/movies";
             }
         }
 
         return "singleMedia";
     }
     @GetMapping("/songs/{id}")
-    public String getSong(@PathVariable("id") int id, Model model){
+    public String getSong(@PathVariable("id") int id, Model model, RedirectAttributes redirectAttributes){
         for (Song s: getBestSongs()) {
             if (s.getId() == id){
                 model.addAttribute("mediaId", s.getId());
                 model.addAttribute("mediaTitle", s.getTitle());
                 model.addAttribute("songArtist", s.getArtist());
+            } else{
+                redirectAttributes.addFlashAttribute("message", "You was serching for a not listed song pls retry");
+                return "redirect:/songs";
             }
         }
 

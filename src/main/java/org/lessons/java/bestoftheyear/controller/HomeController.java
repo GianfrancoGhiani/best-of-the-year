b0f3d687ba6d118783e46b2511438d8918a046a8
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-03-27 14:31:24 +0200

    best + name

--- src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
+++ src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
@@ -1,10 +1,16 @@
 package org.lessons.java.bestoftheyear.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
+    @GetMapping
+    public String home(Model model){
+        model.addAttribute("name", "Gianfranco Ghiani");
+        return "home";
+    }
 }

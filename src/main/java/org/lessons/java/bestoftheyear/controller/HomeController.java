4a8bcad1b441fac65de60b09026509bb7289274d
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-03-27 14:11:36 +0200

    scaffolding

---
+++ src/main/java/org/lessons/java/bestoftheyear/controller/HomeController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.bestoftheyear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+}
